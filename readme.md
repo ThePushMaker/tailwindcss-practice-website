87e2a7c154708c0032393602be21beaf4987b5c3
DSW <elmcwl@gmail.com>
2023-11-23 00:55:12 -0700

    style: finished proyect

---
+++ readme.md
@@ -0,0 +1,4 @@
+# Tailwind + HTML5
+Practice website with a design inspired by the official Nintendo website for Super Mario 3D World + Bowser's Fury.
+##Preview
+![Preview](/public/img/preview.png)
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
         'wave-pattern': 'url(../img/wave-white.png)',
         'wave-pattern-dots': 'url(../img/pattern-white-dots.png)',
         'pattern-characters-red': 'url(../img/pattern-characters-red.png)',
+        'pattern-characters-black': 'url(../img/pattern-characters-black.png)',
         'character-mario': 'url(../img/character-l.png)',
         'wave-pink': 'url(../img/wave-pink.png)',
         'yellow-dots': 'url(../img/pattern-yellow-dots.png)',
